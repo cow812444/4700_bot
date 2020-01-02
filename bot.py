bebeae71b5d23bafd8de124ab893b4f3194e7d8f
cow812444 <cow812444@gmail.com>
2020-01-02 15:06:52 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ bot = commands.Bot(command_prefix='$$')
 bot.load_extension(f'cmds.Lilipoints')
 bot.load_extension(f'cmds.Summoning')
 bot.load_extension(f'cmds.NewsPush')
+bot.load_extension(f'cmds.ColorPalette')
 @bot.event
 async def on_ready():
     print('>> 岡田Bot is online')
