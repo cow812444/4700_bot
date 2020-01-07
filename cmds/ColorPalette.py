d0103835f08fa482e811400f5d33a6f5454b8ca7
cow812444 <cow812444@gmail.com>
2020-01-07 15:46:56 +0800

    Update ColorPalette.py

--- cmds/ColorPalette.py
+++ cmds/ColorPalette.py
@@ -26,12 +26,12 @@ class ColorPalette(Cog_Extension):
             if role:
                 #change roles color
                 await role.edit(hoist=True, colour=discord.Colour.from_rgb(rgb_color[0],rgb_color[1],rgb_color[2]))
-                await self.bot.add_roles(user, role)
+                await user.add_roles(role)
             else:
                 await guild.create_role(name=msg.author.display_name, colour=discord.Colour.from_rgb(rgb_color[0],rgb_color[1],rgb_color[2]))
                 role = get(guild.roles, name=msg.author.display_name)
                 print(role)
-                await self.bot.add_roles(user, role)
+                await user.add_roles(role)
         #msg.author
         pass
     @commands.Cog.listener()
