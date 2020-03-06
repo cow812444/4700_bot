154de72e4b45e9bf9995354f790f41aa12f05777
Lishang <cow812444@gmail.com>
2020-03-06 16:40:26 +0800

    Delete env.py

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-class load_env():
-    #os.environ['DISCORD_TOKEN'] = 'NjQyNDIyMDUzMjIzNTMwNTA2.Xd-__w._AXH0nYbuGcUGrw3RsVH1QC05-o'
-    #os.environ.setdefault("DISCORD_BOT_SECRET", "NjQyNDIyMDUzMjIzNTMwNTA2.Xd-8MA.v6LO1CJyTKTzlHUjsAPZ6IQkZXw")
